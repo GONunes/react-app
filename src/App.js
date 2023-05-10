d023f32958cc04fe57ebfd68a0d80e448f676385
Guilherme Nunes <52090831+GONunes@users.noreply.github.com>
2023-05-09 22:37:43 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Production environment
+          DevOps Básico IT Talent - Mudanças aula 26-04-2023
         </p>
         <a
           className="App-link"
